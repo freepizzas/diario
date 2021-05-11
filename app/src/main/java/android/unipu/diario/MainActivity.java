23961b2e7919b736bedbeaaaddcc272829658860
nikki <6318495+echrs@users.noreply.github.com>
2021-05-11 22:26:52 +0200

    splash screen

--- app/src/main/java/android/unipu/diario/MainActivity.java
+++ app/src/main/java/android/unipu/diario/MainActivity.java
@@ -2,7 +2,10 @@ package android.unipu.diario;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportActionBar().hide();
+        Intent intent = new Intent(this, HomeActivity.class);
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
     }
 }
