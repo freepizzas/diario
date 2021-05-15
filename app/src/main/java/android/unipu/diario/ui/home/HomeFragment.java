e7340b1bcfca2497366284a5cafc921dcd8d7f45
nikki <6318495+echrs@users.noreply.github.com>
2021-05-15 22:33:46 +0200

    question activity

--- app/src/main/java/android/unipu/diario/ui/home/HomeFragment.java
+++ app/src/main/java/android/unipu/diario/ui/home/HomeFragment.java
@@ -1,6 +1,10 @@
 package android.unipu.diario.ui.home;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.unipu.diario.ui.diary.DiaryFragment;
+import android.unipu.diario.ui.diary.DiaryViewModel;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,6 +12,7 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
@@ -17,19 +22,13 @@ import android.unipu.diario.R;
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
+    private CardView writeBtn;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         homeViewModel =
                 new ViewModelProvider(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-//        final TextView textView = root.findViewById(R.id.text_calendar);
-//        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-//            @Override
-//            public void onChanged(@Nullable String s) {
-//                textView.setText(s);
-//            }
-//        });
         return root;
     }
 }
