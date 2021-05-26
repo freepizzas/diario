3241522a2225600d2b2cdd1c9e5929e11ba37c11
starcictoni <57995236+starcictoni@users.noreply.github.com>
2021-05-26 22:14:53 +0200

    Updated Calendar Fragment

--- app/src/main/java/android/unipu/diario/ui/calendar/CalendarFragment.java
+++ app/src/main/java/android/unipu/diario/ui/calendar/CalendarFragment.java
@@ -2,6 +2,7 @@ package android.unipu.diario.ui.calendar;
 
 import android.os.Bundle;
 import android.unipu.diario.ui.entry.EntryListItemFragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -24,6 +25,25 @@ public class CalendarFragment extends Fragment{
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_calendar, container, false);
         calendarView = root.findViewById(R.id.calendarView);
+
+        calendarView.addOnAttachStateChangeListener(new View.OnAttachStateChangeListener() {
+            @Override
+            public void onViewAttachedToWindow(View v) {
+                    DateFormat df = new SimpleDateFormat("dd/MM/yyyy");
+                    Calendar calendar = Calendar.getInstance();
+                    selectedDate = df.format(calendar.getTime());
+                    Bundle bundle = new Bundle();
+                    bundle.putString("selectedDate", selectedDate);
+                    EntryListItemFragment entryListItemFragment = new EntryListItemFragment();
+                    entryListItemFragment.setArguments(bundle);
+                    getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.fragment_layout,
+                            entryListItemFragment).addToBackStack(null).commit();
+            }
+            @Override
+            public void onViewDetachedFromWindow(View v) {
+            }
+        });
+
         calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
             public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
@@ -31,13 +51,13 @@ public class CalendarFragment extends Fragment{
                 Calendar calendar = Calendar.getInstance();
                 calendar.set(year, month, dayOfMonth);
                 selectedDate = df.format(calendar.getTime());
+                Log.i("mess", selectedDate);
                 Bundle bundle = new Bundle();
                 bundle.putString("selectedDate", selectedDate);
                 EntryListItemFragment entryListItemFragment = new EntryListItemFragment();
                 entryListItemFragment.setArguments(bundle);
                 getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.fragment_layout,
                         entryListItemFragment).addToBackStack(null).commit();
-
             }
         });
         return root;
